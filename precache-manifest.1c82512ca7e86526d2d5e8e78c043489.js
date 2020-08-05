c8908bf4e0ca2bf432e1b43cd098d98dad36d408
Sourav Ojha <sojha7724@gmail.com>
2020-08-05 20:45:28 +0530

    Updates

---
+++ precache-manifest.1c82512ca7e86526d2d5e8e78c043489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0b931f9903d855e7d7b6daab742c6d",
+    "url": "/bughunter-99/Voice-controlled-News-Application/index.html"
+  },
+  {
+    "revision": "2a2984b1e95f206c4d9c",
+    "url": "/bughunter-99/Voice-controlled-News-Application/static/css/main.1c1311f4.chunk.css"
+  },
+  {
+    "revision": "0529041b946ab8c12373",
+    "url": "/bughunter-99/Voice-controlled-News-Application/static/js/2.a3c71001.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/bughunter-99/Voice-controlled-News-Application/static/js/2.a3c71001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2984b1e95f206c4d9c",
+    "url": "/bughunter-99/Voice-controlled-News-Application/static/js/main.a383275e.chunk.js"
+  },
+  {
+    "revision": "2e2a1181d8f17983a5a8",
+    "url": "/bughunter-99/Voice-controlled-News-Application/static/js/runtime-main.f2415ed0.js"
+  }
+]);
